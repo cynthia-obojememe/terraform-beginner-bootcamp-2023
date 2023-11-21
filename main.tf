05b8e0babc18e0ba0691f8f2b5a88261867a768f
cynthia Obojememe <cynthia.obojememe@gmail.com>
2023-11-21 10:40:37 +0000

    create s3 bucket

--- main.tf
+++ main.tf
@@ -4,7 +4,15 @@ terraform {
       source = "hashicorp/random"
       version = "3.5.1"
     }
+     aws = {
+      source = "hashicorp/aws"
+      version = "5.26.0"
+    }
   }
+}
+
+provider "aws" {
+  # Configuration options
 }
 
 provider "random" {
@@ -14,6 +22,17 @@ resource "random_string" "bucket_name" {
   length = 16
   special = false
 
+}
+
+
+resource "aws_s3_bucket" "example" {
+  bucket = "my-tf-test-bucket56buket"
+
+  tags = {
+
+    Name     = "My buckett"
+    Environment  = "DEv"
+  }
 }
 
 output "random_bucket_name_id" {
